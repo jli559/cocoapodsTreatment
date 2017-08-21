921beccebda77e8e2dba65f9186a2f3624bb3b84
jli559 <litj@cncn.com>
2017-08-21 14:07:52 +0800

    add project files

---
+++ cocoapodsTreatment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cocoapodsTreatment
+//
+//  Created by jli on 21/08/2017.
+//  Copyright © 2017 jl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
